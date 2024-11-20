a9c9e1160546eca7f6da50cbdacf5668f7dc2f8a
Julien Delavande <Julien.DELAVANDE@student.isae-supaero.fr>
2024-11-20 15:47:45 +0100

    lab4_altegrad

--- Altegrad/tutorials/Lab4/code/part1/code_lab_exploration.py
+++ Altegrad/tutorials/Lab4/code/part1/code_lab_exploration.py
@@ -8,7 +8,18 @@ import numpy as np
 
 
 ############## Task 1
+import networkx as nx
 
+# Task 1: Load the network data
+def load_graph(file_path):
+    # Load the undirected graph, ignoring comment lines starting with "#"
+    G = nx.read_edgelist(
+        file_path, 
+        delimiter='\t', 
+        comments='#', 
+        create_using=nx.Graph()
+    )
+    return G
 
 
 ############## Task 2
